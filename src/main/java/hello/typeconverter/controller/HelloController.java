e6f4b69f972e9666e4dcb508c24c7d3bde483a27
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-07-14 14:23:31 +0900

    typeconverter

--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,9 +1,13 @@
 package hello.typeconverter.controller;
 
+import hello.typeconverter.type.IpPort;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
@@ -12,6 +16,21 @@ public class HelloController {
         String data = request.getParameter("data"); //문자 타입 조회
         Integer intValue = Integer.valueOf(data); //숫자 타입으로 변경
         System.out.println("intValue = " + intValue);
+
+        return "ok";
+    }
+
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        log.info("data = {}", data);
+        System.out.println("data = " + data);
+        return "ok";
+    }
+
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort){
+        System.out.println("ipPort IP = " + ipPort.getIp());
+        System.out.println("ipPort PORT = " + ipPort.getPort());
 
         return "ok";
     }
